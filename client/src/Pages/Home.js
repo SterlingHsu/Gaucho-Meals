14bb326d1902cb7cf1c0b185ed3fa8820acde301
SterlingHsu <sterlinghsu17@gmail.com>
2024-09-06 14:11:46 -0700

    homepage optimization

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-import { useState, useEffect, useCallback, useMemo } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
 const Home = () => {
-  const [primaryItems, setPrimaryItems] = useState([]);
+  const [groupedPrimaryItems, setGroupedPrimaryItems] = useState(() => {
+    const storedGroupedItems = localStorage.getItem("groupedPrimaryItems");
+    return storedGroupedItems ? JSON.parse(storedGroupedItems) : {};
+  });
 
   const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -14,30 +17,38 @@ const Home = () => {
         withCredentials: true,
       });
       // console.log(response.data);
-      setPrimaryItems(response.data.primaryItems);
+      return response.data.primaryItems;
     } catch (err) {
       console.error("Error fetching primary items:", err);
     }
   }, [apiUrl]);
 
-  useEffect(() => {
-    async function fetchData() {
-      await fetchPrimaryItems();
-    }
-
-    fetchData();
-  }, [fetchPrimaryItems]);
-
-  const groupedPrimaryItems = useMemo(() => {
+  const groupItems = (items) => {
     const grouped = {};
-    primaryItems.forEach((hall) => {
+    items.forEach((hall) => {
       if (!grouped[hall.diningHall]) {
         grouped[hall.diningHall] = {};
       }
       grouped[hall.diningHall][hall.mealTime] = hall.primaryItems;
     });
     return grouped;
-  }, [primaryItems]);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedItems = await fetchPrimaryItems();
+      if (fetchedItems) {
+        const groupedItems = groupItems(fetchedItems);
+        setGroupedPrimaryItems(groupedItems);
+        localStorage.setItem(
+          "groupedPrimaryItems",
+          JSON.stringify(groupedItems)
+        );
+      }
+    }
+
+    fetchData();
+  }, [fetchPrimaryItems]);
 
   return (
     <>
@@ -46,36 +57,38 @@ const Home = () => {
         <div className="flex-grow-1 d-flex align-items-center justify-content-center p-3 overflow-auto">
           <div className="container mt-4">
             <div className="row">
-              {Object.entries(groupedPrimaryItems).map(
-                ([diningHall, meals], index) => (
-                  <div key={index} className="col-md-4 mb-4">
-                    <div className="card h-100 shadow-sm">
-                      <div className="card-body">
-                        <h5 className="card-title">{diningHall}</h5>
-                        {Object.entries(meals).map(
-                          ([mealTime, items], mealIndex) => (
-                            <div key={mealIndex} className="mb-3">
-                              <h6 className="card-subtitle mb-2 text-muted text-capitalize">
-                                {mealTime}
-                              </h6>
-                              <div className="d-flex flex-wrap">
-                                {items && items.map((item, i) => (
-                                  <span
-                                    key={i}
-                                    className="badge bg-light text-dark me-2 mb-2"
-                                  >
-                                    {item}
-                                  </span>
-                                ))}
+              {groupedPrimaryItems &&
+                Object.entries(groupedPrimaryItems).map(
+                  ([diningHall, meals], index) => (
+                    <div key={index} className="col-md-4 mb-4">
+                      <div className="card h-100 shadow-sm">
+                        <div className="card-body">
+                          <h5 className="card-title">{diningHall}</h5>
+                          {Object.entries(meals).map(
+                            ([mealTime, items], mealIndex) => (
+                              <div key={mealIndex} className="mb-3">
+                                <h6 className="card-subtitle mb-2 text-muted text-capitalize">
+                                  {mealTime}
+                                </h6>
+                                <div className="d-flex flex-wrap">
+                                  {items &&
+                                    items.map((item, i) => (
+                                      <span
+                                        key={i}
+                                        className="badge bg-light text-dark me-2 mb-2"
+                                      >
+                                        {item}
+                                      </span>
+                                    ))}
+                                </div>
                               </div>
-                            </div>
-                          )
-                        )}
+                            )
+                          )}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )
-              )}
+                  )
+                )}
             </div>
           </div>
         </div>
